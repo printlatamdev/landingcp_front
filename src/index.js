657437de9d7c217fb780741812f7026ac9316dc8
rauda4286 <ivanrauda96@gmail.com>
2025-01-07 09:29:51 -0600

    hash router change

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter , Route, Routes } from "react-router-dom";
 
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
@@ -32,9 +32,9 @@ import Landing from "views/examples/Landing.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter >
     <Routes>
       <Route path="/" exact element={<Landing />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter >
 );
